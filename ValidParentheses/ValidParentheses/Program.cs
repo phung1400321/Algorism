24c7b0bd70545e41ecd4a8527f867715ebcd021e
Phung.Nguyen <phung.nguyen@ptnglobalcorp.com>
2022-09-30 11:20:58 +0700

    Commit

---
+++ ValidParentheses/ValidParentheses/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+namespace ValidParentheses
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            bool result = IsValid("([{}])");
+            Console.WriteLine(result);
+        }
+
+        public static bool IsValid(string s)
+        {
+            int n = -1;
+            while (s.Length != n)
+            {
+                n = s.Length;
+                s = s.Replace("()", "");
+                s = s.Replace("{}", "");
+                s = s.Replace("[]", "");
+            }
+            if (s.Length == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
